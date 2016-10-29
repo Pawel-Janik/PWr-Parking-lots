e12525f7f4a7b3e282afa588fd2c09d33ed469e2
pawelnr1 <pawelnr1@gmail.com>
2016-10-29 09:27:29 +0200

    Parking detail component structure

--- app/static/app/app.component.ts
+++ app/static/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: `<h1>My First Angular App</h1>
+             <parking-detail></parking-detail>
+             `
 })
+
 export class AppComponent { }
 
---
+++ app/static/app/parking-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'parking-detail',
+    template: `
+        <h2>Parking details</h2>
+    `
+})
+export class ParkingDetailComponent {
+}
