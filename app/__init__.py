ba88d6847d25cc69aa6f535d405f3d5003b272e1
Konrad Podlawski <kpodlawski@kretstudios.pl>
2016-10-31 12:35:21 +0100

    Make Python3 compatible, add requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path="/static")
 app.config.from_object('config')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
 
 from app import routes
